c527e40dd39e15de51693fcf4c8882b6b62ae4ec
Frank1963-mpoyi <frankmpoyi63@gmail.com>
2021-12-23 16:26:28 +0200

    add the begin

--- userDefineDS/linkedList/doubly_linked_list.py
+++ userDefineDS/linkedList/doubly_linked_list.py
@@ -28,6 +28,7 @@ class doublyLL:
 
     def printLL_reverse(self):
         # REVERSE DIRECTION
+        print()
         if self.head is None:
             print("Linked list is empty !")
         else:
@@ -38,7 +39,42 @@ class doublyLL:
                 print(n.data, "--->", end="")
                 n = n.pref
 
+    # Insert node when the linkedlist is empty
+    def insert_empty(self, data):
+        if self.head is None:  # if the head is none we have to insert the new node
+            new_node = Node(data)
+            # after creating the new node point the head to the new node
+            self.head = new_node
+        else:
+            print("linked list is not empty ! ")
+
+    # Insert element at the beginning of link list
+    def add_begin(self, data):
+        new_node = Node(data)  # create new node
+        if self.head is None: # check if the node is empty if it is create the new node
+            self.head = new_node
+        else:
+            new_node.nref = self.head  # we assign the head to new node next reference
+            self.head.pref = new_node
+            self.head = new_node
+
+    # Insert element at the end of the link list
+    def add_end(self, data):
+        new_node = Node(data)  # create new node
+        if self.head is None:  # before adding element at the end of the link list you must first check if the llist is empty or not is it empty create the new node and point the head to the new node
+            self.head = new_node
+        else:
+            n = self.head  # if its not empty we assign the first node to n
+            while n.nref is not None:  # n.nref is to travel to the last next reference node if its become null the condition fails and the loop will stop
+                n = n.nref  # after traversing the n become null come out of the loop
+            n.nref = new_node
+            new_node.pref = n
+
+
 ddl1 = doublyLL()
-# ddl1.printLL()
-# ddl1.printLL_reverse()
+ddl1.insert_empty(10)
+ddl1.add_begin(20)
+ddl1.add_end(100)
+ddl1.printLL()
+ddl1.printLL_reverse()
 
